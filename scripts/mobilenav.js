8c6d879e6e239caa36fe4abc6532d2e067d65951
Karl Palmer <KarltroidContact@Gmail.com>
2019-02-05 21:33:04 -0500

    Mobile Nav Added

---
+++ scripts/mobilenav.js
@@ -0,0 +1,10 @@
+function opennav()
+{
+	document.getElementById("mobile-nav-items").style.display = "block";
+	document.getElementById("mainheader").style.zIndex = "10";
+}
+
+function closenav()
+{
+	document.getElementById("mobile-nav-items").style.display = "none";
+}
