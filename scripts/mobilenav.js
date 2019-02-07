db2548851983dec6b517e435bb7ad1c2dcac1a6f
Karl Palmer <KarlPalmer@student.avrtech.com>
2019-02-07 12:37:17 -0500

    Animated mobile nav menu

--- scripts/mobilenav.js
+++ scripts/mobilenav.js
@@ -1,10 +1,30 @@
 function opennav()
 {
 	document.getElementById("mobile-nav-items").style.display = "block";
-	document.getElementById("mainheader").style.zIndex = "10";
+	document.getElementById("mobile-nav-header").style.animation = "slide-in .4s";
+	document.getElementById("Home").style.animation = "slide-in 0.5s";
+	document.getElementById("Maps").style.animation = "slide-in 0.6s";
+	document.getElementById("Places").style.animation = "slide-in 0.7s";
+	document.getElementById("Transit").style.animation = "slide-in 0.8s";
+	document.getElementById("Attire").style.animation = "slide-in 0.9s";
+	document.getElementById("Food").style.animation = "slide-in 1s";
+
 }
 
 function closenav()
+{
+	document.getElementById("mobile-nav-header").style.animation = "slide-out 1s";
+	document.getElementById("Home").style.animation = "slide-out 0.9s";
+	document.getElementById("Maps").style.animation = "slide-out 0.8s";
+	document.getElementById("Places").style.animation = "slide-out 0.7s";
+	document.getElementById("Transit").style.animation = "slide-out 0.6s";
+	document.getElementById("Attire").style.animation = "slide-out 0.5s";
+	document.getElementById("Food").style.animation = "slide-out 0.4s";
+
+	setTimeout(hide, 400);
+}
+
+function hide()
 {
 	document.getElementById("mobile-nav-items").style.display = "none";
 }
