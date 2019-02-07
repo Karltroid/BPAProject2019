d0c77f61798d83c0023dd0f97fc3244f94423c18
Karl Palmer <KarlPalmer@student.avrtech.com>
2019-02-07 13:09:42 -0500

    Adjusted animation and made edit speed easier

--- scripts/mobilenav.js
+++ scripts/mobilenav.js
@@ -1,25 +1,27 @@
+speed = 1;
+
 function opennav()
 {
 	document.getElementById("mobile-nav-items").style.display = "block";
-	document.getElementById("mobile-nav-header").style.animation = "slide-in .4s";
-	document.getElementById("Home").style.animation = "slide-in 0.5s";
-	document.getElementById("Maps").style.animation = "slide-in 0.6s";
-	document.getElementById("Places").style.animation = "slide-in 0.7s";
-	document.getElementById("Transit").style.animation = "slide-in 0.8s";
-	document.getElementById("Attire").style.animation = "slide-in 0.9s";
-	document.getElementById("Food").style.animation = "slide-in 1s";
+	document.getElementById("mobile-nav-header").style.animation = "slide-in " + speed * 0.4 + "s";
+	document.getElementById("Home").style.animation = "slide-in " + speed * 0.5 + "s";
+	document.getElementById("Maps").style.animation = "slide-in " + speed * 0.6 + "s";
+	document.getElementById("Places").style.animation = "slide-in " + speed * 0.7 + "s";
+	document.getElementById("Transit").style.animation = "slide-in " + speed * 0.8 + "s";
+	document.getElementById("Attire").style.animation = "slide-in " + speed * 0.9 + "s";
+	document.getElementById("Food").style.animation = "slide-in " + speed * 1 + "s";
 
 }
 
 function closenav()
 {
-	document.getElementById("mobile-nav-header").style.animation = "slide-out 1s";
-	document.getElementById("Home").style.animation = "slide-out 0.9s";
-	document.getElementById("Maps").style.animation = "slide-out 0.8s";
-	document.getElementById("Places").style.animation = "slide-out 0.7s";
-	document.getElementById("Transit").style.animation = "slide-out 0.6s";
-	document.getElementById("Attire").style.animation = "slide-out 0.5s";
-	document.getElementById("Food").style.animation = "slide-out 0.4s";
+	document.getElementById("mobile-nav-header").style.animation = "slide-out " + speed * 1 + "s";
+	document.getElementById("Home").style.animation = "slide-out " + speed * 0.9 + "s";
+	document.getElementById("Maps").style.animation = "slide-out " + speed * 0.8 + "s";
+	document.getElementById("Places").style.animation = "slide-out " + speed * 0.7 + "s";
+	document.getElementById("Transit").style.animation = "slide-out " + speed * 0.6 + "s";
+	document.getElementById("Attire").style.animation = "slide-out " + speed * 0.5 + "s";
+	document.getElementById("Food").style.animation = "slide-out " + speed * 0.4 + "s";
 
 	setTimeout(hide, 400);
 }
