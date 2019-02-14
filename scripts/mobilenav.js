fc8bbbf571736f7b40f058aa7f121b2c3ba68afc
Karl Palmer <KarltroidContact@Gmail.com>
2019-02-13 21:53:28 -0500

    Finished/Added All Pages & Fixed Mobile Nav Issue

--- scripts/mobilenav.js
+++ scripts/mobilenav.js
@@ -11,12 +11,12 @@ function opennav()
 	document.getElementById("Food").style.animation = "slide-in " + openSpeed * 0.8 + "s";
 	document.getElementById("Transit").style.animation = "slide-in " + openSpeed * 0.9 + "s";
 	document.getElementById("Attire").style.animation = "slide-in " + openSpeed * 1 + "s";
-
-
+	setTimeout(hidecontent, ((closeSpeed * .4) * 1000));
 }
 
 function closenav()
 {
+	
 	document.getElementById("mobile-nav-header").style.animation = "slide-out " + closeSpeed * 1 + "s";
 	document.getElementById("Home").style.animation = "slide-out " + closeSpeed * 0.9 + "s";
 	document.getElementById("Maps").style.animation = "slide-out " + closeSpeed * 0.8 + "s";
@@ -24,12 +24,19 @@ function closenav()
 	document.getElementById("Food").style.animation = "slide-out " + closeSpeed * 0.6 + "s";
 	document.getElementById("Transit").style.animation = "slide-out " + closeSpeed * 0.5 + "s";
 	document.getElementById("Attire").style.animation = "slide-out " + closeSpeed * 0.4 + "s";
+
 	
 
-	setTimeout(hide, ((closeSpeed * .4) * 1000));
+	setTimeout(hidenav, ((closeSpeed * .4) * 1000));
+	document.getElementById("main-content").style.display = "block";
 }
 
-function hide()
+function hidecontent()
+{
+	document.getElementById("main-content").style.display = "none";
+}
+
+function hidenav()
 {
 	document.getElementById("mobile-nav-items").style.display = "none";
 }
